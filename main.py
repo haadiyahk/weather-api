cf369c0ded1c87c9648c2c171d9b1401f5ee00d6
haadiyahk <khanhaadiyah10@gmail.com>
2026-07-01 18:22:16 +0530

    Add URL endpoints for one station and one date, one station and all dates, one station and a year

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
+
 app = Flask("__name__")
 
+# Load and clean up the list of weather stations
 stations = pd.read_csv("data_small\stations.txt", skiprows=17)
 
 @app.route("/")
@@ -17,6 +19,21 @@ def about(station, date):
             "date" : date,
             "temperature" : temperature
             }
+
+@app.route("/api/v1/<station>")
+def station_data(station):
+    filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df["    DATE"].astype(str)
+    result= df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result 
 
 if __name__ == "__main__":
     app.run(debug=True)
